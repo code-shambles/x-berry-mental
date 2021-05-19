6fd05186a1f4f4ec685efe8c69b9af4f72b43c11
Sebastian Eck <codeshambles@gmail.com>
2021-05-19 19:41:16 +0200

    feat: add dingleberry

---
+++ fruitgums/dingleberry.js
@@ -0,0 +1,7 @@
+module.exports = {
+  color: "brown",
+  flavor: "dingleberry",
+  shape: "oval",
+  size: "s",
+  surface: "smooth",
+}
--- fruitgums/index.js
+++ fruitgums/index.js
@@ -1,6 +1,7 @@
 const blackberry = require("./blackberry.js")
 const blackCurrant = require("./black-currant.js")
 const cranberry = require("./cranberry.js")
+const dingleberry = require("./dingleberry.js")
 const gooseberry = require("./gooseberry.js")
 const kiwi = require("./kiwi.js")
 const raspberry = require("./raspberry.js")
@@ -13,6 +14,7 @@ module.exports = {
   blackberry,
   blackCurrant,
   cranberry,
+  dingleberry,
   gooseberry,
   kiwi,
   raspberry,
